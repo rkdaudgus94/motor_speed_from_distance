2791bd3790d6d3dbd71a0d721754da69c156f242
rkdaudgus94 <76949032+rkdaudgus94@users.noreply.github.com>
2022-07-19 14:28:03 +0900

    1

--- control_ino(hyun).py
+++ control_ino(hyun).py
@@ -1,10 +1,12 @@
 import serial
 
 
+
 port = ''
 brate = 115200
 ard = serial.Serial(port, brate, timeout=None)
-
+dis = []
+spd = []
 print('1을 입력하면 작동')
 
 while True :
@@ -12,13 +14,17 @@ while True :
 
     if (num == '1'):
         num = num.encode('utf-8')
-        ard.write(num)
+        ard.write(num) # num 의 값을 아두이노에 전송
+        time.sleep(0.1)
 
     
     distance = ard.readline()
-    distance = data[:-2].decode()
+    distance = distance[:-2].decode()
     print(distance)
+    dis = int(distance)
+    
 
-    speed = ard.readline()
-    speed = data[:-2].decode
-    print(speed)
+    # speed = ard.readline()
+    # speed = speed[:-2].decode()
+    # print(speed)
+    # spd = int(speed)
--- ultrasonic_waves_motor.ino
+++ ultrasonic_waves_motor.ino
@@ -7,46 +7,55 @@
 
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(TRIG, OUTPUT);
-  pinMode(ECHO, INPUT);
-  pinMode(driverIn3, OUTPUT);
-  pinMode(driverIn4, OUTPUT);
-  pinMode(driverPwmR, OUTPUT);
+   
+    Serial.begin(115200);
+    pinMode(TRIG, OUTPUT);
+    pinMode(ECHO, INPUT);
+    pinMode(driverIn3, OUTPUT);
+    pinMode(driverIn4, OUTPUT);
+    pinMode(driverPwmR, OUTPUT);
+    pinMode(A0, INPUT);
+    pinMode(A1, INTPUT);
+  }
+
+  void loop() {
+    while (Serial.avaliable()) {
+    data = Serial.read(); // 데이터를 읽어서 'data'를 저장
+    }
+    if (data == '1') {
+      digitalWrite(TRIG, LOW);
+      delay(500);
+      digitalWrite(TRIG, HIGH);
+      delay(500);
+      digitalWrite(TRIG, LOW);
+
+      long distance = pulseIn(ECHO, HIGH) / 58.2;
+      Serial.println(distance) // 데이터를 시리얼 통신으로 전송
+
+      if (distance < 10) {
+        digitalWrite(driverIn3, HIGH);
+        digitalWrite(driverIn4, LOW);
+        analogWrite(driverPwmR, 0);
+      }
+      else if (distance < 20) {
+        digitalWrite(driverIn3, HIGH);
+        digitalWrite(driverIn4, LOW);
+        analogWrite(driverPwmR, 50);
+      }
+      else if (distance < 30) {
+        digitalWrite(driverIn3, HIGH);
+        digitalWrite(driverIn4, LOW);
+        analogWrite(driverPwmR, 100);
+      }
+      else if (distance > 30) {
+        digitalWrite(driverIn3, HIGH);
+        digitalWrite(driverIn4, LOW);
+        analogWrite(driverPwmR, 200);
+      }
+      Serial.println(driverPwmR)
+      Serial.print(distance);
+      Serial.print("cm");
+
+      delay(100);
 }
-
-void loop() {
-  digitalWrite(TRIG, LOW);
-  delay(500);
-  digitalWrite(TRIG, HIGH);
-  delay(500);
-  digitalWrite(TRIG, LOW);
-  
-  long distance = pulseIn(ECHO, HIGH) / 58.2;
-
-  if (distance < 10) {
-    digitalWrite(driverIn3, HIGH);
-    digitalWrite(driverIn4, LOW);
-    analogWrite(driverPwmR, 0);
-  }
-  else if (distance < 20) {
-    digitalWrite(driverIn3, HIGH);
-    digitalWrite(driverIn4, LOW);
-    analogWrite(driverPwmR, 50);
-  }
-  else if (distance < 30) {
-    digitalWrite(driverIn3, HIGH);
-    digitalWrite(driverIn4, LOW);
-    analogWrite(driverPwmR, 100);
-  }
-  else if (distance > 30) {
-    digitalWrite(driverIn3, HIGH);
-    digitalWrite(driverIn4, LOW);
-    analogWrite(driverPwmR, 200);
-  }
-
-  Serial.print(distance);
-  Serial.print("cm");
-
-  delay(100);
 }
