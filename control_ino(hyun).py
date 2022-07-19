3885cb9ae6b3619638c5a7ea57a61f9007898b5f
ssun0402 <109329261+ssun0402@users.noreply.github.com>
2022-07-19 15:09:56 +0900

    Update control_ino(hyun).py

--- control_ino(hyun).py
+++ control_ino(hyun).py
@@ -1,30 +1,28 @@
 import serial
+import time
 
 
-
-port = ''
+port = 'COM3'
 brate = 115200
 ard = serial.Serial(port, brate, timeout=None)
 dis = []
 spd = []
 print('1을 입력하면 작동')
 
+num = input()
+dis.append(num)
 while True :
-    num = input()
-
-    if (num == '1'):
-        num = num.encode('utf-8')
-        ard.write(num) # num 의 값을 아두이노에 전송
-        time.sleep(0.1)
-
+    num = dis[0].encode('utf-8')
+    ard.write(num) # num 의 값을 아두이노에 전송
+    time.sleep(0.1)
     
     distance = ard.readline()
     distance = distance[:-2].decode()
     print(distance)
     dis = int(distance)
     
-
+    
     # speed = ard.readline()
     # speed = speed[:-2].decode()
-    # print(speed)
+    # print(speed)1
     # spd = int(speed)
