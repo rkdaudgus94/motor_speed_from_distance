43c2160414ba46d459cb8b094ae56578b4875acb
ssun0402 <109329261+ssun0402@users.noreply.github.com>
2022-07-19 16:08:01 +0900

    Update control_ino(seon).py

--- control_ino(seon).py
+++ control_ino(seon).py
@@ -1,2 +1,17 @@
 # 파이썬 -> 아두이노 (모터 구동)
 
+import serial
+
+baudrate = 115200
+arduino = serial.Serial('com3', 115200, timeout = None)
+
+while True :
+    setha = input("각도(setha)를 입력하시오 : ")
+
+    setha = setha.encode()
+    arduino.write(setha)
+    data = arduino.readline()
+    data = data.decode()
+
+    print("setha : ", setha)
+    print(data)
