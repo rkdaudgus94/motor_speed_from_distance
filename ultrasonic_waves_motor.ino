e973b2cbc66af6cb0f5c72b84c39c1b492736409
ssun0402 <109329261+ssun0402@users.noreply.github.com>
2022-07-20 19:42:51 +0900

    Update ultrasonic_waves_motor.ino

--- ultrasonic_waves_motor.ino
+++ ultrasonic_waves_motor.ino
@@ -8,61 +8,66 @@
 int data;
 
 void setup() {
-   
-    Serial.begin(9600);
-    pinMode(TRIG, OUTPUT);
-    pinMode(ECHO, INPUT);
-    pinMode(driverIn3, OUTPUT);
-    pinMode(driverIn4, OUTPUT);
-    analogRead(driverPwmR);
-  }
+
+  Serial.begin(9600);
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT);
+  pinMode(driverIn3, OUTPUT);
+  pinMode(driverIn4, OUTPUT);
+  pinMode(driverPwmR, OUTPUT);
+}
 
 void loop() {
-    while (Serial.available()) {
-      data = Serial.read(); // 데이터를 읽어서 'data'를 저장
-    }
+  if (Serial.available()) {
+    data = Serial.read(); // 데이터를 읽어서 'data'를 저장
+
     while (data == '1') {
       digitalWrite(TRIG, LOW);
       delay(10);
       digitalWrite(TRIG, HIGH);
       delay(10);
       digitalWrite(TRIG, LOW);
-    
-      float distance = pulseIn(ECHO, HIGH) / 58.8;
-     
 
-      if (distance < 10) {
+      float distance = pulseIn(ECHO, HIGH) / 58.8;
+
+      if (Serial.available()) {
+        data = Serial.read();
+
+        if (data == '0') {
+          digitalWrite(driverIn3, LOW);
+          digitalWrite(driverIn4, LOW);
+          analogWrite(driverPwmR, 0); //pwm = 0
+          break;
+        }
+      }
+
+      else if (distance < 10) {
         digitalWrite(driverIn3, HIGH);
         digitalWrite(driverIn4, LOW);
-        analogWrite(driverPwmR, 0);
+        analogWrite(driverPwmR, 0); // 0% Duty Cycle;
         // pwm = 0;
-        
+
       }
       else if (distance < 20) {
         digitalWrite(driverIn3, HIGH);
         digitalWrite(driverIn4, LOW);
-        analogWrite(driverPwmR, 50); // pwm = 50;
+        analogWrite(driverPwmR, 64); // 25% Duty Cycle;
       }
       else if (distance < 30) {
         digitalWrite(driverIn3, HIGH);
         digitalWrite(driverIn4, LOW);
-        analogWrite(driverPwmR, 125); // pwm = 125;
+        analogWrite(driverPwmR, 127); // 50% Duty Cycle;
       }
       else if (distance > 30) {
         digitalWrite(driverIn3, HIGH);
         digitalWrite(driverIn4, LOW);
-        analogWrite(driverPwmR, 255); //pwm = 255;
-      }
-       else if (data == '0') {
-        digitalWrite(driverIn3, LOW);
-        digitalWrite(driverIn4, LOW);
-        analogWrite(driverPwmR, 0); //pwm = 0
-        break;
+        analogWrite(driverPwmR, 255); // 100% Duty Cycle;
       }
       Serial.print(distance);
-      Serial.print(" ");
+      Serial.print("  ");
       Serial.println(driverPwmR);
 
-      delay(100);
+      delay(500);
     }
+  }
 }
