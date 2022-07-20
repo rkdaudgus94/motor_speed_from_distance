3ce2e26b24650fcfb360ea3fdcb2fb3acd91f59d
ssun0402 <109329261+ssun0402@users.noreply.github.com>
2022-07-20 21:55:36 +0900

    Update ultrasonic_waves_motor.ino

--- ultrasonic_waves_motor.ino
+++ ultrasonic_waves_motor.ino
@@ -5,9 +5,16 @@
 #define driverIn4 10
 #define driverPwmR 11
 
+int data;
+int data1;
+int pwm;
+
+float distance;
+
+void distance_motor();
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   pinMode(TRIG, OUTPUT);
   pinMode(ECHO, INPUT);
   pinMode(driverIn3, OUTPUT);
@@ -16,37 +23,58 @@ void setup() {
 }
 
 void loop() {
+  if (Serial.available()) {
+    data = Serial.read(); // 데이터를 읽어서 'data'를 저장
+  }
+  while (data == '1') {
+    Serial.available();
+    data1 = Serial.read();
+    distance_motor();
+
+    if (data1 == '0') {
+      Serial.print("break");
+      break;
+    }
+  }
+}
+
+void distance_motor() {
   digitalWrite(TRIG, LOW);
-  delayMicroseconds(2);
+  delay(10);
   digitalWrite(TRIG, HIGH);
-  delayMicroseconds(10);
+  delay(10);
   digitalWrite(TRIG, LOW);
 
-  long distance = pulseIn(ECHO, HIGH) / 58.2;
+  distance = pulseIn(ECHO, HIGH) / 58.8;
 
   if (distance < 10) {
     digitalWrite(driverIn3, HIGH);
     digitalWrite(driverIn4, LOW);
-    analogWrite(driverPwmR, 0);
+    analogWrite(driverPwmR, 0); // 0% Duty Cycle
+    pwm = 0;
   }
   else if (distance < 20) {
     digitalWrite(driverIn3, HIGH);
     digitalWrite(driverIn4, LOW);
-    analogWrite(driverPwmR, 50);
+    analogWrite(driverPwmR, 64); // 25% Duty Cycle
+    pwm = 25;
   }
   else if (distance < 30) {
     digitalWrite(driverIn3, HIGH);
     digitalWrite(driverIn4, LOW);
-    analogWrite(driverPwmR, 100);
+    analogWrite(driverPwmR, 127); // 50% Duty Cycle
+    pwm = 50 ;
   }
-  else if (distance > 30) {
+  else  {
     digitalWrite(driverIn3, HIGH);
     digitalWrite(driverIn4, LOW);
-    analogWrite(driverPwmR, 200);
+    analogWrite(driverPwmR, 255); // 100% Duty Cycle
+    pwm = 100;
   }
 
   Serial.print(distance);
   Serial.print("cm");
+  Serial.println(pwm);
 
-  delay(100);
+  delay(1000);
 }
