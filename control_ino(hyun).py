5d1348207f5de43910ec371211cc65e0687e1889
rkdaudgus94 <76949032+rkdaudgus94@users.noreply.github.com>
2022-07-20 20:10:30 +0900

    Update control_ino(hyun).py

--- control_ino(hyun).py
+++ control_ino(hyun).py
@@ -1,7 +1,8 @@
 import serial
 import time
-import matplotlib as plot
+import matplotlib.pyplot as plt
 import numpy as np
+from drawnow import *
 
 
 port = 'COM5'
@@ -10,23 +11,45 @@ ard = serial.Serial(port, brate)
 dis = []
 spd = []
 t = []
+
+def show_plot() :
+    plt.subplot(211)
+    plt.plot(t,dis,label='distance')
+    plt.legend('distance(cm)')
+    plt.grid()
+    plt.xlabel('t(sec)')
+    plt.ylabel('distance(cm)')
+
+    plt.subplot(212)
+    plt.plot(t,spd,label='speed')
+    plt.legend('speed(%)')
+    plt.grid()
+    plt.xlabel('t(sec)')
+    plt.ylabel('speed(%)')
+    
+
+
 print('1을 입력하면 작동')
 num= input()
+
+
 try : 
     while True :
         num1 = num.encode('utf-8')
         ard.write(num1) # num 의 값을 아두이노에 전송
         while num == '1' :
             data = ard.readline()
-            distance = data.decode()[:-6]
+            distance = data.decode()[:-7]
             speed = data.decode()[-4:-2]
             distance = float(distance)
             speed = float(speed)
             print(distance,"cm",speed,'(pwm)')
-            t= t.append(0.5)
-            print(t)
+            dis = np.append(dis,distance)
+            spd = np.append(spd,speed)
+            t = np.append(t,0.5)
+            
             # dis = int(distance)
-
+            drawnow(show_plot)
 except KeyboardInterrupt :
     num = 0
     ard.write(num)
