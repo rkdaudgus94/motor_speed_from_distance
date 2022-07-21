5f037a75b2f3446402929992fd96a06beec71e7b
ssun0402 <109329261+ssun0402@users.noreply.github.com>
2022-07-21 13:00:57 +0900

    Update ultrasonic_waves_motor.ino

--- ultrasonic_waves_motor.ino
+++ ultrasonic_waves_motor.ino
@@ -5,9 +5,14 @@
 #define driverIn4 10
 #define driverPwmR 11
 
+int data;
+int pwm;
+float distance;
+
+void distance_motor();
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   pinMode(TRIG, OUTPUT);
   pinMode(ECHO, INPUT);
   pinMode(driverIn3, OUTPUT);
@@ -16,37 +21,109 @@ void setup() {
 }
 
 void loop() {
+  if (Serial.available()) {
+    data = Serial.read(); // 데이터를 읽어서 'data'를 저장
+  }
+  while (data == '1') {
+    distance_motor();
+    if (Serial.available()) {
+      data = Serial.read();
+
+      if (data == '0') {
+        Serial.println("END");
+        break;
+      }
+    }
+  }
+}
+
+void distance_motor() {
   digitalWrite(TRIG, LOW);
-  delayMicroseconds(2);
+  delay(10);
   digitalWrite(TRIG, HIGH);
-  delayMicroseconds(10);
+  delay(10);
   digitalWrite(TRIG, LOW);
 
-  long distance = pulseIn(ECHO, HIGH) / 58.2;
+  distance = pulseIn(ECHO, HIGH) / 58.8;
 
   if (distance < 10) {
     digitalWrite(driverIn3, HIGH);
     digitalWrite(driverIn4, LOW);
-    analogWrite(driverPwmR, 0);
+    analogWrite(driverPwmR, 0); // 0% Duty Cycle
+    pwm = 0;
+    Serial.print("   ");
+    Serial.print(distance);
+    Serial.print("cm");
+    Serial.print("   ");
+    Serial.println(pwm);
   }
   else if (distance < 20) {
     digitalWrite(driverIn3, HIGH);
     digitalWrite(driverIn4, LOW);
-    analogWrite(driverPwmR, 50);
+    analogWrite(driverPwmR, 64); // 25% Duty Cycle
+    pwm = 25;
+    Serial.print("  ");
+    Serial.print(distance);
+    Serial.print("cm");
+    Serial.print("  ");
+    Serial.println(pwm);
   }
   else if (distance < 30) {
     digitalWrite(driverIn3, HIGH);
     digitalWrite(driverIn4, LOW);
-    analogWrite(driverPwmR, 100);
+    analogWrite(driverPwmR, 127); // 50% Duty Cycle
+    pwm = 50 ;
+    Serial.print("  ");
+    Serial.print(distance);
+    Serial.print("cm");
+    Serial.print("  ");
+    Serial.println(pwm);
   }
-  else if (distance > 30) {
+  else if (distance < 100) {
     digitalWrite(driverIn3, HIGH);
     digitalWrite(driverIn4, LOW);
-    analogWrite(driverPwmR, 200);
+    analogWrite(driverPwmR, 255); // 100% Duty Cycle
+    pwm = 100;
+    Serial.print("  ");
+    Serial.print(distance);
+    Serial.print("cm");
+    Serial.print(" ");
+    Serial.println(pwm);
   }
 
-  Serial.print(distance);
-  Serial.print("cm");
+  else if (distance < 1000) {
+    digitalWrite(driverIn3, HIGH);
+    digitalWrite(driverIn4, LOW);
+    analogWrite(driverPwmR, 255); // 100% Duty Cycle
+    pwm = 100;
+    Serial.print(" ");
+    Serial.print(distance);
+    Serial.print("cm");
+    Serial.print(" ");
+    Serial.println(pwm);
+  }
+  else if (distance < 10000) {
+    digitalWrite(driverIn3, HIGH);
+    digitalWrite(driverIn4, LOW);
+    analogWrite(driverPwmR, 255); // 100% Duty Cycle
+    pwm = 100;
+    Serial.print(distance);
+    Serial.print("cm");
+    Serial.print(" ");
+    Serial.println(pwm);
+  }
 
-  delay(100);
+  else if (distance > 10000) {
+    distance = pulseIn(ECHO, LOW);
+    digitalWrite(driverIn3, LOW);
+    digitalWrite(driverIn4, LOW);
+    analogWrite(driverPwmR, 0); // 100% Duty Cycle
+    pwm = 0;
+    Serial.print("   ");
+    Serial.print(distance);
+    Serial.print("cm");
+    Serial.print("   ");
+    Serial.println(pwm);
+  }
+  delay(500);
 }
